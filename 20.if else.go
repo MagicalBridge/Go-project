88602d629c7ca30fbcfbf022256c2c2a250a8502
褚鹏飞 <540788769@qq.com>
2022-07-27 10:46:22 +0800

    feat: 循环结构

---
+++ 20.if else.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 18
+
+	if age >= 18 {
+		fmt.Println("已经成年了")
+	} else {
+		fmt.Println("还没有成年")
+	}
+}
---
+++ 21.循环结构.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 循环条件不加括号，感觉好别扭呀
+func main() {
+	var i int
+	for i = 1; i <= 10; i++ {
+		fmt.Println("为什么循环不加括号啊")
+	}
+
+	var j int
+	var sum int
+
+	for j = 1; j <= 100; j++ {
+		sum = sum + j
+	}
+
+	fmt.Println("sum", sum)
+}
