27d04636fa10c97499aeffb945610763c5b20967
褚鹏飞 <540788769@qq.com>
2022-11-22 15:22:38 +0800

    添加go函数不定参数列表

--- 22.函数的参数个数.go
+++ 22.函数的参数个数.go
@@ -15,7 +15,26 @@ func SumAdd() {
 }
 
 
+func SumAdd2(num int) {
+	var sum int
+
+	for i:=1; i<= num ; i++ {
+		sum += i
+	}
+
+	fmt.Println(sum)
+}
+
+// 定义两个形参
+// 调用的时候传递两个实参，实参和形参的个数保持一致 类型也要保持一致
+func add(num1 int ,num2 int)  {
+	fmt.Println(num1 + num2)
+}
+
+
 func main()  {
 	SumAdd()
-
+	SumAdd2(200)
+	SumAdd2(300)
+	add(8,7)
 }
