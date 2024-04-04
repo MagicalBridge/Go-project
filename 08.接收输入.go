e917decf49094558914ee7c27f8b519d653d9f8c
chupengfei <450793585@qq.com>
2024-04-04 19:05:11 +0800

    浮点类型的值

--- 08.接收输入.go
+++ 08.接收输入.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
-
-// scanf函数将键盘输入的数据赋值给变量 但是变量前一定要加 & 符号
+/*
+*
+scanf 函数将键盘输入的数据赋值给变量 但是变量前一定要加 & 符号
+scan 这个函数使用起来和 scanf 很类似，而且要简单一些，在以后的编程过程中使用这个居多
+*/
 func main() {
 	var age int
 	fmt.Println("请输入年龄：")
+	// 第一个参数代表的是我想输入的是整数类型，赋值给哪个变量的内存地址
+	// 前面需要加上&，这点很重要
 	fmt.Scanf("%d", &age)
-	fmt.Println("age=",age)
-	fmt.Println(&age)  // 0xc0000b4008 打印的是内存地址
+	fmt.Println("age=", age)
+	fmt.Println(&age) // 0xc0000b4008 打印的是内存地址
+
 }
--- 10.浮点类型.go
+++ 10.浮点类型.go
@@ -2,15 +2,16 @@ package main
 
 import "fmt"
 
-// 建议使用float64 来声明浮点类型
-// 默认保留的是6位小数，我们可以控制显示小数的位数
+// 在go语言中，提供了两种浮点数据类型 一个是float32，一个是float64
+// 建议使用float64 来声明浮点类型，因为一些函数库中都是使用float64的
+// float32 精确到小数点后面7位，float64 精确到小数点后面15位
 // 这里有一点需要注意，自动推导类型创建的变量中，创建浮点类型，默认为float64
+
 func main() {
 	var num float64
 	var num2 float64
 	num = 123.45
 	num2 = 123.4567
 	fmt.Printf("%f \n", num) // 123.450000
-
 	fmt.Printf("%.2f", num2) // 123.46
 }
