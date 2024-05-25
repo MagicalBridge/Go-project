6dd416def9bb30ceb393cafe1ea91a0121d2b842
chupengfei <450793585@qq.com>
2024-05-25 18:17:54 +0800

    feat: add the func defination

--- 22.函数的参数个数.go
+++ 22.函数的参数个数.go
@@ -7,18 +7,17 @@ import "fmt"
 func SumAdd() {
 	var sum int
 
-	for i:=1; i<= 100 ; i++ {
+	for i := 1; i <= 100; i++ {
 		sum += i
 	}
 
 	fmt.Println(sum)
 }
 
-
 func SumAdd2(num int) {
 	var sum int
 
-	for i:=1; i<= num ; i++ {
+	for i := 1; i <= num; i++ {
 		sum += i
 	}
 
@@ -27,14 +26,13 @@ func SumAdd2(num int) {
 
 // 定义两个形参
 // 调用的时候传递两个实参，实参和形参的个数保持一致 类型也要保持一致
-func add(num1 int ,num2 int)  {
+func add(num1 int, num2 int) {
 	fmt.Println(num1 + num2)
 }
 
-
-func main()  {
+func main() {
 	SumAdd()
 	SumAdd2(200)
 	SumAdd2(300)
-	add(8,7)
+	add(8, 7)
 }
--- 23.函数不定参数列表.go
+++ 23.函数不定参数列表.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+// 不定参数的形式更加灵活，相当于一个集合，会给参数添加一个编号，是从0开始算的
+
+func TestSum(args ...int) {
+	fmt.Println(args[0])
+	fmt.Println(args[1])
+	fmt.Println(args[2])
+}
+
+func main() {
+	TestSum(1, 2, 4)
+}
