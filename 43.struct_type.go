bc3862fdc0c9bc12bb64cf9db9d355aed1dd1837
chupengfei <450793585@qq.com>
2024-08-11 10:15:24 +0800

    feat: add some comment Comments

--- 43.struct_type.go
+++ 43.struct_type.go
@@ -4,7 +4,8 @@ import "fmt"
 
 func main() {
 	// 结构体是由一系列具有相同类型或不同类型的数据组成的数据集合
-	//结构体可以很好的管理一批有联系的数据，使用结构体可以提高程序的易读性
+	// 结构体可以很好的管理一批有联系的数据，使用结构体可以提高程序的易读性
+
 	// 成员名称前不能添加var
 	type Student struct {
 		id   int
@@ -23,6 +24,7 @@ func main() {
 
 	fmt.Println(s) // {10 btc 12 china}
 
+	// 部分初始化
 	var s1 = Student{
 		name: "btc",
 		addr: "china",
@@ -30,5 +32,5 @@ func main() {
 
 	s1.id = 23 // 可以使用具体的成员变量名称赋值
 
-	fmt.Println(s1) // {0 btc 0 china}
+	fmt.Println(s1) // {23 btc 0 china}
 }
