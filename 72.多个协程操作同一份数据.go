890e192fda7a1767f566a9bcbcf282be947c497c
chupengfei <450793585@qq.com>
2024-10-07 11:20:06 +0800

    feat: use lock

--- 72.多个协程操作同一份数据.go
+++ 72.多个协程操作同一份数据.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 下面这个例子演示的是多个协程操作的同一份的数据，输出的结果和我们的预期不太一样
+// 最后打印的结果比较随机，思考下为什么会出现这样的问题，
+// 如何解决这个问题：确保一个协程在执行逻辑的时候另外的协程不执行
+var totalNum int
+var wg2 = sync.WaitGroup{}
+
+func add2() {
+	defer wg2.Done()
+	for i := 0; i < 100000; i++ {
+		totalNum = totalNum + 1
+	}
+}
+
+func sub() {
+	defer wg2.Done()
+	for i := 0; i < 100000; i++ {
+		totalNum = totalNum - 1
+	}
+}
+
+func main() {
+	wg2.Add(2)
+	go add2()
+	go sub()
+	wg2.Wait()
+
+	fmt.Println(totalNum)
+}
---
+++ 73.互斥锁的引入.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// 为了解决多个携程冲突的问题，我们给协程加上一个锁的机制
+var totalNum2 int
+var wg3 = sync.WaitGroup{}
+var lock = sync.Mutex{}
+
+func add3() {
+	defer wg3.Done()
+	for i := 0; i < 100000; i++ {
+		lock.Lock()
+		totalNum2 = totalNum2 + 1
+		lock.Unlock()
+	}
+}
+
+func sub3() {
+	defer wg3.Done()
+	for i := 0; i < 100000; i++ {
+		lock.Lock()
+		totalNum2 = totalNum2 - 1
+		lock.Unlock()
+	}
+}
+
+func main() {
+	wg3.Add(2)
+	go add3()
+	go sub3()
+	wg3.Wait()
+
+	// 加上锁之后，无论执行多少次，结果总是0
+	fmt.Println(totalNum2)
+}
