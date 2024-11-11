21a398498eef517ece2734da1772ac8694070560
chupengfei <450793585@qq.com>
2024-11-11 21:22:08 +0800

    feat: complate the demo code

--- interview/1-交替打印数字和字母.go
+++ interview/1-交替打印数字和字母.go
@@ -6,38 +6,52 @@ import (
 )
 
 func main() {
-	numChan := make(chan bool)
-	letterChan := make(chan bool)
+	// 初始化 sync.Cond 和互斥锁
+	var mutex sync.Mutex
+	cond := sync.NewCond(&mutex)
 
+	// 控制打印顺序的标志
+	printNum := true
+
+	// 使用 WaitGroup 等待 goroutine 完成
 	var wg sync.WaitGroup
 	wg.Add(2)
 
+	// 定义打印数字的 goroutine
 	go func() {
-		defer wg.Done()
-		for i := 1; i <= 28; i += 2 {
-			<-letterChan
-			fmt.Printf("%d%d", i, i+1)
-			numChan <- true
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		letters := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-		letterIndex := 0
-
-		for range numChan {
-			if letterIndex >= len(letters) {
-				break
+		defer wg.Done() // 在 goroutine 完成后通知 WaitGroup
+		for i := 1; i <= 28; i++ {
+			cond.L.Lock()
+			for !printNum {
+				cond.Wait()
 			}
-			fmt.Printf("%c%c", letters[letterIndex], letters[letterIndex+1])
-			letterIndex += 2
-			letterChan <- true
+			fmt.Printf("%d", i)
+			if i < 28 {
+				i++
+				fmt.Printf("%d", i)
+			}
+			printNum = false
+			cond.Signal()
+			cond.L.Unlock()
 		}
 	}()
 
-	// Start the process
-	letterChan <- true
+	// 定义打印字母的 goroutine
+	go func() {
+		defer wg.Done() // 在 goroutine 完成后通知 WaitGroup
+		for i := 'A'; i <= 'Z'; i++ {
+			cond.L.Lock()
+			for printNum {
+				cond.Wait()
+			}
+			fmt.Printf("%c%c", i, i+1)
+			i++
+			printNum = true
+			cond.Signal()
+			cond.L.Unlock()
+		}
+	}()
 
+	// 等待两个 goroutine 执行完毕
 	wg.Wait()
 }
